e9bff0b1376a88fc917506c5cd0daa43e9b79b3a
hannes-walden <hannes.walden@outlook.com>
2025-01-20 14:35:10 +0200

    ensimmäinen React soveltava tehtävä.

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+}
+
+const Header = (props) => {
+  console.log("Header-komponentti")
+  console.log(props)
+  console.log(props.course)
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+
+
+export default App
