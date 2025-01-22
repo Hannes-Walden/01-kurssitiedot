56beb6ce78d366a24a96e57c7b8abae1d4f5a35a
hannes-walden <hannes.walden@outlook.com>
2025-01-22 14:34:25 +0200

    JavaScriptiä lisätty

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+ ]
 }
 
 const Header = (props) => {
@@ -66,8 +75,8 @@ return (
     <Header course={course}/>
     <Content
     part1 = {part1}
-    part = {part2}
-    part = {part3}
+    part2 = {part2}
+    part3 = {part3}
     exercises1 = {exercises1}
     exercises2 = {exercises2}
     exercises3 = {exercises3}
@@ -76,5 +85,6 @@ return (
 
   </div>
 )
+}
 
 export default App
